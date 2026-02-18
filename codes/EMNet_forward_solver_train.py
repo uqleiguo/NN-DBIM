72bf9ed02a9cf2ac7355c97136a1fc19aff076c5
lei guo_mac_2 <uqlguo3@MacBook-Pro.local>
2026-02-19 01:22:44 +1000

    Add codes for the EMNet Part 1 - Forward solver

--- codes/EMNet_forward_solver_train.py
+++ codes/EMNet_forward_solver_train.py
@@ -4,22 +4,11 @@ import torch.nn as nn
 import torch.optim as optim
 import utils
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-train_dataset = torch.load('../data/MaxwellNet_Unet_rand_heads_with_real_heads_train_data.pt')
-test_dataset = torch.load('../data/MaxwellNet_Unet_rand_heads_with_real_heads_test_data.pt')
-FD_Ez_inc = torch.from_numpy(np.load('../data/Alex_Ali_Phong_EMV_FDTD_sim_FD_Ez_inc.npy'))
-Gezz = torch.from_numpy(np.load('../data/Gezz.npy'))
-
-FD_Ez_inc = torch.mean(FD_Ez_inc, dim=2)
-Ez_inc_real = torch.real(FD_Ez_inc).expand(1, 1, 128, 128)
-Ez_inc_imag = torch.imag(FD_Ez_inc).expand(1, 1, 128, 128)
-Ez_inc_new = torch.cat((Ez_inc_real, Ez_inc_imag), dim=1).to(device, torch.float)
-
-GT_Nx = 128
-GT_Ny = 128
-
-channels = np.array([64, 128, 256, 512, 1024])
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
 
 
 class DoubleConv(nn.Module):
@@ -111,15 +100,8 @@ class UNet(nn.Module):
         return x_tot, x_scat, e_tot, e_scat
 
 
-model = UNet(out_channels=2, channel_num=channels, e_inc=Ez_inc_new).to(device)
-criterion = nn.MSELoss()
-
-
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
-
-
-print(f'The model has {count_parameters(model):,} trainable parameters')
 
 
 def train(model, train_set, Ez_inc, Gezz, loss_weight_1, loss_weight_2, loss_weight_3, optimizer, criterion, clip):
@@ -184,76 +166,40 @@ def train(model, train_set, Ez_inc, Gezz, loss_weight_1, loss_weight_2, loss_wei
     return epoch_loss / len(train_set)
 
 
-def evaluate(model, test_set, Ez_inc):
+if __name__ == '__main__':
 
-    model.eval()
+    train_dataset = torch.load('../data/forward_solver_train_data.pt', weights_only=False)
 
-    ep = torch.zeros(len(test_set), 2, 128, 128)
-    ep_gt = torch.zeros(len(test_set), 2, 128, 128)
-    rmse = torch.zeros(len(test_set), 2)
-    e_tot = torch.zeros(len(test_set), 2, 128, 128)
+    FD_Ez_inc = torch.from_numpy(np.load('../data/ez_inc.npy'))
+    Gezz = torch.from_numpy(np.load('../data/Gezz.npy'))
 
-    with torch.no_grad():
-        for ii, data in enumerate(test_set):
-            src_EP = data[0].to(device, torch.float)
+    FD_Ez_inc = torch.mean(FD_Ez_inc, dim=2)
+    Ez_inc_real = torch.real(FD_Ez_inc).expand(1, 1, 128, 128)
+    Ez_inc_imag = torch.imag(FD_Ez_inc).expand(1, 1, 128, 128)
+    Ez_inc_new = torch.cat((Ez_inc_real, Ez_inc_imag), dim=1).to(device, torch.float)
 
-            Ez_inc_real = torch.real(Ez_inc).expand(src_EP.shape[0], 1, Ez_inc.shape[0], Ez_inc.shape[1])
-            Ez_inc_imag = torch.imag(Ez_inc).expand(src_EP.shape[0], 1, Ez_inc.shape[0], Ez_inc.shape[1])
-            Ez_inc_new = torch.cat((Ez_inc_real, Ez_inc_imag), dim=1).to(device, torch.float)
+    GT_Nx = 128
+    GT_Ny = 128
 
-            src = torch.cat((src_EP, Ez_inc_new), dim=1)
+    channels = np.array([64, 128, 256, 512, 1024])
 
-            trg = data[1].to(device, torch.float)
+    model = UNet(out_channels=2, channel_num=channels, e_inc=Ez_inc_new).to(device)
+    criterion = nn.MSELoss()
+    CLIP = 1
+    learning_rate = 1e-5
+    loss_weight_1 = 0.2
+    loss_weight_2 = 0.1
+    loss_weight_3 = 0.2
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-            y_tot, y_scat, recon_e_tot, recon_e_scat = model(src)
+    print(f'The model has {count_parameters(model):,} trainable parameters')
 
-            ep[ii, :, :, :] = utils.curl_curl_operator_2d_ep(recon_e_tot[0, :, :, :].expand(1, 2, 128, 128))
+    for epoch in range(1, 10001):
 
-            ep_gt[ii, 0, :, :] = trg[0, 0, :, :]
-            ep_gt[ii, 1, :, :] = trg[0, 1, :, :]
+        train_loss = train(model, train_dataset, FD_Ez_inc, Gezz, loss_weight_1, loss_weight_2, loss_weight_3, optimizer, criterion, CLIP)
+        print(f"Train Loss at the {epoch}th epoch is {train_loss}\n")
 
-            rmse[ii, 0] = torch.sqrt(torch.sum((ep[ii, 0, :, :] - ep_gt[ii, 0, :, :]) ** 2) /
-                                     torch.sum(ep_gt[ii, 0, :, :] ** 2))
-            rmse[ii, 1] = torch.sqrt(torch.sum((ep[ii, 1, :, :] - ep_gt[ii, 1, :, :]) ** 2) /
-                                     torch.sum(ep_gt[ii, 1, :, :] ** 2))
-
-            e_tot[ii, :, :, :] = recon_e_tot[0, :, :, :]
-
-        rmse = torch.mean(rmse, 0)
-
-    return ep, ep_gt, e_tot, rmse
-
-
-CLIP = 1
-learning_rate = 1e-5
-loss_weight_1 = 0.4
-loss_weight_2 = 0.1
-loss_weight_3 = 0.4
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-epoch_rmse = np.zeros((200, 2))
-epoch_cnt = 1
-
-for epoch in range(1, 10001):
-    # if epoch % 5000 == 0:
-    #     learning_rate = learning_rate / 1.25
-    #     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-    train_loss = train(model, train_dataset, FD_Ez_inc, Gezz, loss_weight_1, loss_weight_2, loss_weight_3, optimizer, criterion, CLIP)
-    print(f"Train Loss at the {epoch}th epoch is {train_loss}\n")
-
-    if epoch % 50 == 0:
-        _, _, _, rmse_tmp = evaluate(model, test_dataset, FD_Ez_inc)
-        epoch_rmse[epoch_cnt - 1, :] = rmse_tmp.cpu().numpy()
-
-        print(f"Test RMSE at the {epoch}th epoch is {epoch_rmse[epoch_cnt - 1, :]}\n")
-
-        epoch_cnt += 1
-
-        torch.save(model.state_dict(), 'MaxwellNet_EP_mapping_models/model_Unet_no_shim_' + str(epoch) + '_v26.pkl')
-
-
-np.save('../results/epoch_rmse_Unet_rand_heads_v26', epoch_rmse)
-print('debug')
+    print('debug')
 
 
 
